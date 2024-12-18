ff602493a4b41501bc6ec5d8befc8d08b7f4ec2d
domy838 <domy@tlu.ee>
2024-12-18 17:33:04 +0200

    Enemy bowman finished

--- Assets/Scripts/Projectile/ProjectileBehaviour.cs
+++ Assets/Scripts/Projectile/ProjectileBehaviour.cs
@@ -7,18 +7,19 @@ public class ProjectileBehaviour : MonoBehaviour
     public float moveSpeed = 5f;
     public float destroyDelay = 5f;
     private float direction;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.Find("Player");
 		// Another form of the Destroy function, which allows us to destroy an object
 		// after a delay in seconds. We set the delay with a variable "destroyAfter"
         Destroy(gameObject, destroyDelay);
         // Did we or the enemy shoot?
-        if(transform.position.y > -4)
+        if(transform.position.y != player.transform.position.y)
         {
-            // If enemy shoots, the arrow goes in opposite direction and rotated
-            direction = -1f;
+            // If enemy shoots, the arrow goes in opposite direction
             Vector3 toRotate = new Vector3(0, 0, 180);
             transform.Rotate(toRotate);
             gameObject.tag = "EnemyArrow";
@@ -26,7 +27,6 @@ public class ProjectileBehaviour : MonoBehaviour
         else
         {
             // If we shoot, the arrow goes in the intendet directionS
-            direction = 1f;
             gameObject.tag = "Laser";
         }
     }
@@ -37,7 +37,7 @@ public class ProjectileBehaviour : MonoBehaviour
         // Moves the game object along the Y-axis (X is 0f), and we make the Y value into 
         // a variable so we can change the direction (up or down) and make the script reusable
         // in different situations. (can do the same for X and then it'll move in any direction you want)
-        Vector3 translationVector = new Vector3(0f, 1f) * moveSpeed * Time.deltaTime * direction;
+        Vector3 translationVector = new Vector3(0f, 1f) * moveSpeed * Time.deltaTime;
         transform.Translate(translationVector);
     }
 }
